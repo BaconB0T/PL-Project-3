2d70eecfe4b6777a33366bdfe1ab60afd32b184a
@EthanStyle <brownev19@gcc.edu>
2023-04-07 17:20:53 -0400

    Update PageSearchTestSuite.scala
    Updated test values to count substrings, not exact matches. Numbers were gotten by highlighting each webpage in testPages.csv and searching within selection for "subroutine", "language", and "asd"

--- src/test/scala/PageSearchTestSuite.scala
+++ src/test/scala/PageSearchTestSuite.scala
@@ -11,8 +11,13 @@ class PageSearchTest extends AnyFunSuite with Matchers with BeforeAndAfterAll {
         rankedPages = PageRank.makeRankedPages(pages, PageRank.equal)
     }
     test("Count should return a list of the number of times any of the terms appeared in each page in the same order as given") {
-        assert(PageSearch.count(this.rankedPages, List("subroutine")) == List(0, 1, 4, 1, 0))
-        assert(PageSearch.count(this.rankedPages, List("language")) == List(12, 6, 13, 29, 14))
+        assert(PageSearch.count(this.rankedPages, List("subroutine")) == List(0, 4, 4, 5, 0))
+        assert(PageSearch.count(this.rankedPages, List("language")) == List(11, 6, 14, 28, 14))
         assert(PageSearch.count(this.rankedPages, List("asd")) == List(0, 0, 0, 0, 0))
     }
+//    test("makeSearchedWebPage should return a list of searchedWebPages") {
+//        val textmatch: List[Double] = PageSearch.count(this.rankedPages, List("subroutine"))
+//        assert(PageSearch.makeSearchedPages(this.rankedPages, textmatch)
+//
+//    }
 }
