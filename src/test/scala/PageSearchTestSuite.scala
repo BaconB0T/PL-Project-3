456b3ebda188c87f6b9351cd718209d4b0aff686
@EthanStyle <brownev19@gcc.edu>
2023-04-07 22:25:06 -0400

    Part A done + Test file

--- src/test/scala/PageSearchTestSuite.scala
+++ src/test/scala/PageSearchTestSuite.scala
@@ -8,11 +8,13 @@ class PageSearchTest extends AnyFunSuite with Matchers with BeforeAndAfterAll {
         val testFileName: String = "testPages.csv"
 
         pages = mapWebPages(loadWebPages(testFileName))
-        rankedPages = PageRank.makeRankedPages(pages, PageRank.equal)
+        rankedPages = PageRank.makeRankedPages(pages, PageRank.equal).sortBy(_.id)
+
+        assert(pages.toList.length == rankedPages.length)
     }
     test("Count should return a list of the number of times any of the terms appeared in each page in the same order as given") {
-        assert(PageSearch.count(this.rankedPages, List("subroutine")) == List(0, 4, 1, 5, 0))
-        assert(PageSearch.count(this.rankedPages, List("language")) == List(11, 6, 14, 28, 14))
+        assert(PageSearch.count(this.rankedPages, List("subroutine")) == List(0.0, 4.0, 5.0, 1.0, 0.0))
+        assert(PageSearch.count(this.rankedPages, List("language")) == List(11.0, 6.0, 28.0, 14.0, 14.0))
         assert(PageSearch.count(this.rankedPages, List("asd")) == List(0, 0, 0, 0, 0))
     }
 //    test("makeSearchedWebPage should return a list of searchedWebPages") {
